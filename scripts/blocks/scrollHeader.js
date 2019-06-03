cd2f720464ad7919f780717527f7ae946aad1e32
loremlipsum <lipsum.webdevelop@gmail.com>
2019-06-03 16:49:14 +0300

    deploy

--- scripts/blocks/scrollHeader.js
+++ scripts/blocks/scrollHeader.js
@@ -3,10 +3,11 @@
 	$(function() {
     var header = $('.js-header-scroll');
     var lastScrollTop = 0;
+    var SCROLL_HEIGHT = 200;
 
     $(window).scroll(function(e){
       var st = $(this).scrollTop();
-      if (st > lastScrollTop) {
+      if (st > lastScrollTop || st < SCROLL_HEIGHT) {
         header.removeClass('active');
       } else  {
         header.addClass('active');
