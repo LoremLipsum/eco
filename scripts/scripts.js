6b430847af744111bc416e4bc57f43c7cc25f18e
loremlipsum <lipsum.webdevelop@gmail.com>
2019-06-16 13:22:53 +0300

    deploy

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -14,6 +14,32 @@
   });
 })(jQuery);
 
+(function($) {
+	'use strict';
+	$(function() {
+    var btn = $('.js-page-up');
+    var lastScrollTop = 0;
+    var SCROLL_HEIGHT = 200;
+
+    $(window).scroll(function(e){
+      var st = $(this).scrollTop();
+      if (st > lastScrollTop || st < SCROLL_HEIGHT) {
+        btn.removeClass('active');
+      } else  {
+        btn.addClass('active');
+      }
+      lastScrollTop = st;
+    });
+
+    btn.click(function (){
+      $('html, body').animate({
+        scrollTop: 0
+      }, 500);
+    });
+
+	});
+})(jQuery);
+
 'use strict';
 
 (function() {
